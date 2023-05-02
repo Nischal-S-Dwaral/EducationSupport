33fd99e554181ac0e3e9d2ee1b5da1b2ebc957e9
Sid Soton <srr1n22@soton.ac.uk>
2023-05-02 17:49:50 +0100

    firebase integration with educator home

--- app/src/main/java/com/example/educationsupport/fragment/educator/AddNewLearnerFragment.kt
+++ app/src/main/java/com/example/educationsupport/fragment/educator/AddNewLearnerFragment.kt
@@ -1,9 +1,12 @@
 package com.example.educationsupport.fragment.educator
 
+import android.app.SearchManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+//import android.widget.ArrayAdapter
+import android.widget.ListView
 import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -13,14 +16,23 @@ import com.example.educationsupport.adapters.learner.CourseCardAdapter
 import com.example.educationsupport.adapters.learner.LearnerSearchAdapter
 import com.example.educationsupport.constants.CourseListConstants
 import com.example.educationsupport.constants.LearnerListConstants
+import android.widget.ArrayAdapter
+
+
 
 class AddNewLearnerFragment : Fragment() {
 
-    private lateinit var learnerListRecyclerView: RecyclerView
-    private lateinit var learnerListLayoutManager: RecyclerView.LayoutManager
-    private lateinit var LearnerSearchAdapter: LearnerSearchAdapter
-    private lateinit var searchView: SearchView
-//    private lateinit var courseListAdapter: CourseCardAdapter
+    // creating variables for listview
+    lateinit var programmingLanguagesLV: ListView
+
+    // creating array adapter for listview
+    lateinit var listAdapter: ArrayAdapter<String>
+
+    // creating array list for listview
+    lateinit var programmingLanguagesList: ArrayList<String>;
+
+    // creating variable for searchview
+    lateinit var searchView: SearchView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -31,32 +43,63 @@ class AddNewLearnerFragment : Fragment() {
 
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.educator_add_learner, container, false)
-//        courseListAdapter = CourseCardAdapter(courseList, view.context)
-        LearnerSearchAdapter = LearnerSearchAdapter(learnerList, view.context)
-        learnerListLayoutManager = LinearLayoutManager(view.context)
-//        learnerListRecyclerView = view.findViewById(R.id.rv_learner_list_cards)
-//        learnerListRecyclerView.setHasFixedSize(true)
+
         /**
          * Search view filtering
          */
-        searchView = view.findViewById(R.id.search_view_learner_list)
+
+        // initializing variables of list view with their ids.
+        programmingLanguagesLV = view.findViewById(R.id.idLVProgrammingLanguages)
+        searchView = view.findViewById<SearchView>(R.id.idSV)
+
+        // initializing list and adding data to list
+        programmingLanguagesList = ArrayList()
+        programmingLanguagesList.add("Learner1")
+        programmingLanguagesList.add("Learner2")
+        programmingLanguagesList.add("Learner3")
+        programmingLanguagesList.add("Learner4")
+        programmingLanguagesList.add("5")
+        programmingLanguagesList.add("6")
+        programmingLanguagesList.add("7")
+
+
+
+        // initializing list adapter and setting layout
+        // for each list view item and adding array list to it.
+        listAdapter = ArrayAdapter<String>(
+            requireContext(),
+            android.R.layout.simple_list_item_1,
+            programmingLanguagesList
+        )
+
+        // on below line setting list
+        // adapter to our list view.
+        programmingLanguagesLV.adapter = listAdapter
+
         searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-
             override fun onQueryTextSubmit(query: String?): Boolean {
-                LearnerSearchAdapter.filter.filter(query)
+                // on below line we are checking
+                // if query exist or not.
+                if (programmingLanguagesList.contains(query)) {
+                    // if query exist within list we
+                    // are filtering our list adapter.
+                    listAdapter.filter.filter(query)
+                } else {
+                    // if query is not present we are displaying
+                    // a toast message as no  data found..
+//                    Toast.makeText(this@MainActivity, "No Language found..", Toast.LENGTH_LONG)
+//                        .show()
+                }
                 return false
             }
-
             override fun onQueryTextChange(newText: String?): Boolean {
-                LearnerSearchAdapter.filter.filter(newText)
+                // if query text is change in that case we
+                // are filtering our adapter with
+                // new text on below line.
+                listAdapter.filter.filter(newText)
                 return false
             }
-
         })
-
-//        learnerListRecyclerView.layoutManager = learnerListLayoutManager
-//        learnerListRecyclerView.adapter = LearnerSearchAdapter
-
         return view
     }
 }
--- app/src/main/java/com/example/educationsupport/fragment/educator/EducatorHomeFragment.kt
+++ app/src/main/java/com/example/educationsupport/fragment/educator/EducatorHomeFragment.kt
@@ -1,22 +1,41 @@
 package com.example.educationsupport.fragment.educator
 
+import android.content.ContentValues
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.educationsupport.R
 import com.example.educationsupport.adapters.educator.EducatorCourseListCardAdapter
+import com.example.educationsupport.adapters.learner.CourseCardAdapter
 import com.example.educationsupport.constants.EducatorCourseConstants
+import com.example.educationsupport.model.Course
+import com.google.firebase.database.*
 
 class EducatorHomeFragment : Fragment() {
+    private lateinit var educatorCourseListRecyclerView: RecyclerView
+    private lateinit var educatorCoursesListLayoutManager: RecyclerView.LayoutManager
+    private lateinit var educatorCoursesListAdaptor: EducatorCourseListCardAdapter
+    private lateinit var educatorCourseList: ArrayList<Course>
+    private lateinit var view: View
+    private lateinit var databaseReference: DatabaseReference
 
-    private lateinit var myCourseListRecyclerView: RecyclerView
-    private lateinit var myCoursesListLayoutManager: RecyclerView.LayoutManager
-    //TODO: Different Adapter the Educator Course Details
-    private lateinit var myCoursesListAdaptor: EducatorCourseListCardAdapter
+//    private lateinit var courseListRecyclerView: RecyclerView
+//    private lateinit var courseListLayoutManager: RecyclerView.LayoutManager
+//    private lateinit var courseListAdapter: CourseCardAdapter
+//
+//    private lateinit var databaseReference: DatabaseReference
+//    private lateinit var courseList: ArrayList<Course>
+//
+//    private lateinit var searchView: SearchView
+//    private lateinit var view: View
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,23 +43,58 @@ class EducatorHomeFragment : Fragment() {
     ): View? {
 
         // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.educator_fragment_home, container, false)
+        view = inflater.inflate(R.layout.fragment_educator_courses, container, false)
 
         /**
-         * Get the enrolled course list
+         * Get the educator course list
          */
-        val educatorCourseList = EducatorCourseConstants.educatorCourseList()
+//        val educatorCourseList = EducatorCourseConstants.educatorCourseList();
+//        val educatorCourseList = getEducatorCourses()
 
+        educatorCourseList = getEducatorCourses() as ArrayList<Course>
         /**
          * Set up my courses recycler view
          */
-        myCoursesListLayoutManager = LinearLayoutManager(view.context)
-        myCoursesListAdaptor = EducatorCourseListCardAdapter(educatorCourseList, view.context)
-        myCourseListRecyclerView = view.findViewById(R.id.educator_rv_educator_courses_list_cards)
-        myCourseListRecyclerView.setHasFixedSize(true)
-        myCourseListRecyclerView.layoutManager = myCoursesListLayoutManager
-        myCourseListRecyclerView.adapter = myCoursesListAdaptor
+
+        educatorCoursesListLayoutManager = LinearLayoutManager(view.context)
+        educatorCourseListRecyclerView = view.findViewById(R.id.rv_educator_courses_list_cards)
+        educatorCourseListRecyclerView.setHasFixedSize(true)
 
         return view
+    }
+
+    private fun getEducatorCourses(): ArrayList<Course> {
+        databaseReference = FirebaseDatabase.getInstance().getReference("Courses")
+        educatorCourseList = arrayListOf<Course>()
+        databaseReference.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                educatorCourseList.clear()
+                if(snapshot.exists()){
+                    for(courseSnap in snapshot.children){
+                        val courseData = courseSnap.getValue(Course::class.java)
+                        educatorCourseList.add(courseData!!)
+                    }
+
+                    educatorCoursesListAdaptor = EducatorCourseListCardAdapter(educatorCourseList, view.context)
+
+                    educatorCourseListRecyclerView.layoutManager = educatorCoursesListLayoutManager
+                    educatorCourseListRecyclerView.adapter = educatorCoursesListAdaptor
+//                    myCoursesListAdaptor = EducatorCourseListCardAdapter(educatorCourseList, view.context)
+//                    myCourseListRecyclerView.adapter = myCoursesListAdaptor
+                    //give me code to print the educatorCourseList
+                    educatorCourseList.forEach { println(it) }
+
+//                    myCourseListRecyclerView.visibility = View.VISIBLE
+                }
+
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+                Log.w(TAG, "loadPost:onCancelled", error.toException())
+            }
+        })
+        return educatorCourseList
     }
 }
